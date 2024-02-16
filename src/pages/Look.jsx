a745fb9d3ea065573df79670af7118d828e73187
Rereyre <rereyre.rere@gmail.com>
2024-02-16 11:28:25 +0700

    slider

--- src/pages/Look.jsx
+++ src/pages/Look.jsx
@@ -1,9 +1,72 @@
 import React from "react";
+import Slider from "react-slick";
 import NavbarIbb from "../components/NavbarIbb";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import lookLogo from "../assets/lookLogo.png";
+import ReactCardSlider from "react-card-slider-component";
+import lookAero1 from "../assets/lookcycle 2024_Page_6_Image_0001.jpg";
+import lookAero2 from "../assets/lookcycle 2024_Page_7_Image_0001.jpg";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "lightblue" }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "lightblue" }}
+      onClick={onClick}
+    />
+  );
+}
 
 const Look = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
     <>
       <Container className="bannerLook" fluid>
@@ -86,8 +149,47 @@ const Look = () => {
       </Container>
       <Container className="space" fluid>
         <Row>
-          <Col className="p-2" style={{ textAlign: "center", color: "#fff" }}>AERO</Col>
+          <Col className="p-2" style={{ textAlign: "center", color: "#fff" }}>
+            AERO
+          </Col>
         </Row>
+      </Container>
+      <Container>
+        <div className="slider-container">
+          <Slider {...settings}>
+            <div>
+              <h3>
+                <img src={lookAero1} width={280} />
+              </h3>
+            </div>
+            <div>
+              <h3>
+                <img src={lookAero2} width={280} />
+              </h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            <div>
+              <h3>4</h3>
+            </div>
+            <div>
+              <h3>5</h3>
+            </div>
+            <div>
+              <h3>6</h3>
+            </div>
+            <div>
+              <h3>7</h3>
+            </div>
+            <div>
+              <h3>8</h3>
+            </div>
+            <div>
+              <h3>9</h3>
+            </div>
+          </Slider>
+        </div>
       </Container>
     </>
   );
